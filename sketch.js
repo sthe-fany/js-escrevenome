cf5a7c6cc8c029b2b8f5219a98b4c59eae5f584d
sthe-fany <sthefanyvih9258@gmail.com>
2024-06-06 18:32:17 -0300

    adicionando arquivos Js e css

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
-function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-  }
+function setup() { 
+    createCanvas(600, 600);
+     background("black");
+     }
+   
+   function draw(){ 
+    stroke ("blue")
+     fill ("red")
+   
+    if(mouseIsPressed){ 
+     rect(mouseX, mouseY, 20,35);
+    }
+     
+     }   
